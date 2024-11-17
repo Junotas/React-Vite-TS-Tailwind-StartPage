e76867c8ce6b47ab226a03f28d2a4a848cbe4cad
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-11-17 23:52:33 +0100

    removed the backend techstack from the ts file as those aren't used in this project

--- src/data/techStackData.ts
+++ src/data/techStackData.ts
@@ -1,33 +1,9 @@
-import JavaIcon from "../assets/techstack/java.svg";
-import SpringIcon from "../assets/techstack/spring-icon.svg";
-import DockerIcon from "../assets/techstack/docker-icon.svg";
-import GithubActionsIcon from "../assets/techstack/github-actions.svg";
 import NodeIcon from "../assets/techstack/nodejs-logo-svgrepo-com.svg";
 import TypeScriptIcon from "../assets/techstack/typescript-svgrepo-com.svg";
-import Vite from "../assets/TechStack/vite-svgrepo-com.svg"
+import Vite from "../assets/TechStack/vite-svgrepo-com.svg";
 import TanStackIcon from "../assets/techstack/logo-color-600w-Bx4vtR8J.png";
 
 export const techStackData = [
-  {
-    name: "Java 21",
-    icon: JavaIcon,
-    url: "https://openjdk.org/projects/jdk/21/",
-  },
-  {
-    name: "Spring",
-    icon: SpringIcon,
-    url: "https://spring.io/",
-  },
-  {
-    name: "Docker",
-    icon: DockerIcon,
-    url: "https://www.docker.com/",
-  },
-  {
-    name: "Github Actions",
-    icon: GithubActionsIcon,
-    url: "https://github.com/features/actions",
-  },
   {
     name: "Node.js",
     icon: NodeIcon,
@@ -40,7 +16,7 @@ export const techStackData = [
   },
   {
     name: "React-vite",
-    icon: Vite, 
+    icon: Vite,
     url: "https://vitejs.dev/",
   },
   {
